f7038aaa55284bfa3f06cf95ca0a3d8d0186d3fe
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-12-15 22:59:16 -0500

    Add random name picker

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -6,9 +6,14 @@ import Helpers from './helpers.js';
 
 var App = () => {
   var cohortStudents = Helpers.students;
+  var randomName = cohortStudents[Math.floor(Math.random() * cohortStudents.length)];
+
     return(
       <div className="App">
         <h1> RPP2209 Github </h1>
+        <button type="button" onClick={() => {
+          alert(randomName.name)
+        }}>Random Name Picker</button>
         <div className="classRoom">
           <StudentList list={cohortStudents} link={cohortStudents}/>
         </div>
